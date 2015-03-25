570b63c2220ab85aa31c48b689477ee15cdfea41
CarlosML27 <CarlosML27@outlook.com>
2015-03-25 20:47:27 +0100

    Empty project

---
+++ src/main/java/prDoublyLinkedList/DoublyLinkedList/DLList.java
@@ -0,0 +1,11 @@
+package prDoublyLinkedList.DoublyLinkedList;
+
+/**
+ * Hello world!
+ *
+ */
+public class DLList {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
